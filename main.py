7f75771edb695ccf2bc98e49845a8bb580e3eaa0
YassinWael <yassinwaelmohamed84@gmail.com>
2025-03-12 17:25:49 +0300

    got reddit info with api, airtable too

--- main.py
+++ main.py
@@ -2,59 +2,42 @@ from icecream import ic
 from pyairtable import Api
 from os import environ
 from dotenv import load_dotenv
-import requests
-from datetime import datetime
-from time import sleep
+import praw
+load_dotenv()
+client_id = environ.get("client_id")
+client_secret = environ.get("client_secret")
+airtable_api = environ.get("airtable_token")
 
 
-def determine_status(user_data):
+reddit = praw.Reddit(
+    client_id = client_id,
+    client_secret = client_secret,
+    user_agent="test by u/narutominecraft1"  # Custom User-Agent (change this)
+)
+api = Api(airtable_api)
+table = api.table(table_name="reddit",base_id="appgEjnGN8uQYjgjq")
 
-    if user_data.get("is_suspended"):
-        status = "suspended"
-    elif user_data['subreddit']['user_is_banned']:
-        status = "banned"
-    else:
-        status = "active"
-    return status
 
 
 def get_reddit_info(username):
-    
-    """
-    Makes a request to the Reddit API to get a user's information.
+    user = reddit.redditor(username)
+    status = "active" # default.
+    if hasattr(user,"is_suspended"):
+        ic("suspended alert!")
+        status = "suspended"
 
-    Parameters
-    ----------
-   """
-    headers = {"User-Agent":"Mozilla/5.0"}
-    url = f"https://www.reddit.com/user/{username}/about.json"
-    response = requests.get(url,headers)
 
-    if response.status_code == 200:
-        user_data = response.json()['data']
-        ic(user_data)
-        user_info = {
-            "karma":user_data['total_karma'],
-            "status":determine_status(user_data)
-        }
+    user_info = {
+        "username":user.name,
+        "status":status,
+        "total karma":user.total_karma,
 
-        ic(user_info)
-    elif response.status_code == 429:
-        ic("Rate limited, trying again.")
-        sleep(2)
-        get_reddit_info(username)
-    else:
-        ic(response.status_code)
+    }
+    ic(user_info)
     return user_info
 
 
+naruto = get_reddit_info("myvirginityisstrong")
 
-
-
-
-# load_dotenv()
-# airtable_api = environ.get("airtable_token")
-# ic(airtable_api)
-# api = Api(airtable_api)
-# table = api.table(table_name="reddit",base_id="appgEjnGN8uQYjgjq")
-# print(table.all())
+table.create(naruto)
+# ToDo: Analyse the posts to get the date of the last released one.
